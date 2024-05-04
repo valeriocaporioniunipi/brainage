80abd0059812f17c55c62739590b486d9306c13b
dottordepie <jacoresa@libero.it>
2024-05-04 11:41:24 +0200

    changing of --location functionality

--- code/neural_net.py
+++ code/neural_net.py
@@ -94,7 +94,7 @@ def main():
     parser = argparse.ArgumentParser(description='Neural network predicting the age of patients from magnetic resonance imaging')
 
     parser.add_argument("filename", help="Name of the file that has to be analized")
-    parser.add_argument("--location", default='data', help="Location of the file, i.e. folder containing it")
+    parser.add_argument("--location", help="Location of the file, i.e. folder containing it")
     parser.add_argument("epochs", help="Number of epochs of training")
     parser.add_argument("--summary", action="store_true", help="Show the summary of the neural network")
     parser.add_argument("--history", action="store_true", help="Show the history of the training")
