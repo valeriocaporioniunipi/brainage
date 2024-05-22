28efbef360c648135dd0ff27bf5ddfe09aca5962
Valerio Caporioni <valeriocaporioni@gmail.com>
2024-05-22 17:02:24 +0200

    importing modules in plots

--- plots/oversampling_hist.py
+++ plots/oversampling_hist.py
@@ -1,6 +1,8 @@
+import sys
+import os
 import matplotlib.pyplot as plt
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from brainage.utils import get_data
-
 
 _, targets = get_data(r'../data/FS_features_ABIDE_males_someGlobals.csv',
                          'AGE_AT_SCAN', 5, overs = False)
@@ -9,9 +11,11 @@ _, targets_os = get_data(r'../data/FS_features_ABIDE_males_someGlobals.csv',
 
 plt.figure(figsize=(10, 6))
 
-plt.hist(targets, bins=30, color='blue', alpha=0.5, edgecolor='black', label='Original targets')
+plt.hist(targets, bins=30, color='blue',
+         alpha=0.5, edgecolor='black', label='Original targets')
 
-plt.hist(targets_os, bins=30, color='green', alpha=0.5, edgecolor='black', label='Oversampled targets')
+plt.hist(targets_os, bins=30, color='green',
+         alpha=0.5, edgecolor='black', label='Oversampled targets')
 
 plt.title('Oversampling on ages')
 plt.xlabel('Age')
